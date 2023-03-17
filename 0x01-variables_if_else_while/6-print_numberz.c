ad3d719a79f54f8d7dec29662f2d9652b354a1b8
sofiaares <sofiaares@github.com>
2023-03-17 14:08:37 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i);
+		putchar(i + '0');
 	putchar('\n');
 	return (0);
 }
