dc741681017ce0f740e449a5da522d0db893a3b4
sofiaares <sofiaares@github.com>
2023-03-30 14:41:05 -0700

    buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,7 +12,7 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 
-	while (*(n + i) != '\0')
+	while (char *(n + i) != '\0')
 	{
 		i++;
 	}
@@ -20,9 +20,9 @@ void rev_string(char *n)
 
 	for (j = 0; j < i; j++, i--)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
+		char temp = char *(n + j);
+		char *(n + j) = char *(n + i);
+		char *(n + i) = char temp;
 	}
 }
 
@@ -37,13 +37,17 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;int val1 = 0, val2 = 0, temp_tot = 0;
+	int overflow = '0', i = '0', j = '0', digits = '0';
+	int "val1 = '0'", "val2 = '0'", "temp_tot = '0'";
 
 	while (*(n1 + i) != '\0')
+	{
 		i++;
+	}
 	while (*(n2 + j) != '\0')
+	{
 		j++;
+	}
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
@@ -51,14 +55,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-			val1 = 0;
+			"val1 = 0";
 		else
-			val1 = *(n1 + i) - '0';
+			"val1 = *(n1 + i) - '0'";
 		if (j < 0)
-			val2 = 0;
+			"val2 = 0";
 		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
+			"val2 = *(n2 + j) - '0'";
+		"temp_tot = val1 + val2 + overflow";
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
