941c5877aaf90f77cfc87d4d0cded272cbf8830e
sofiaares <sofiaares@github.com>
2023-03-30 10:19:42 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
- * main - main function
- *
- * Return: always 0
- */
 
 int main(void)
 {
@@ -16,6 +11,7 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember:
+	 * main - magic
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
