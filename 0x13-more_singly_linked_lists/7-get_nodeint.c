05847cb8fa1aa031cd61f4a656ed2c1b2d7160dd
sofiaares <sofiaares@github.com>
2023-05-01 05:25:51 -0700

    e

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,12 +4,14 @@
  * get_nodeint_at_index - returns the nth node of a list
  * @head: parameter
  * @index: parameter
+ * Return: pointer to the node we're looking for, or NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *sofia = head;
+
 	while (sofia && i < index)
 	{
 		sofia = sofia->next;
