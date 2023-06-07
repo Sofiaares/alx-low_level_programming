80e83601ea4ee91c086361d9975483323cc1b4ed
Sofiaares <sofiaares@github.com>
2023-06-07 06:15:13 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - output of characters and strings that prints a string in reverse
+ * @s: string
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion (s + 1);
+		putchar (*s);
+	}
+	else
+		putchar ('\n');
+}
