b3d7abec180578ceaf4d23f3f93f8f71345c63f4
Sofiaares <sofiaares@github.com>
2023-06-07 10:27:33 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,8 +24,9 @@ int _prime(int n, int i)
 {
 	if (n == (2 * i) + 1)
 		return (1);
-	else
-		if (n % i == 0 && i > 0)
-			return (0);
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
 	return (_prime(n, i - 1));
 }
