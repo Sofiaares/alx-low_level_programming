4480f95670bbf5f5b63edf50ddcc280df649a0be
Sofiaares <sofiaares@github.com>
2023-06-07 06:56:34 -0700

    return

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - calculate the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+	if (*s)
+	{
+		length = _strlen_recursion(s + 1);
+		length++;
+	}
+	return length;
+}
