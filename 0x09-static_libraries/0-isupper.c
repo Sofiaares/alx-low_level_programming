686c073d0df8286305cfff2f13e6f39a0f557634
Sofiaares <sofiaares@github.com>
2023-06-09 10:10:07 -0700

    library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - check the code for Holberton School student
+ * @c: integer parameter
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
