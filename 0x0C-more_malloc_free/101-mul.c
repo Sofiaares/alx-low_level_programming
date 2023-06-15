fed63111243b375ad9fa4e80633dd47d2fba5827
Sofiaares <sofiaares@github.com>
2023-06-15 05:21:46 -0700

    more

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * bc - An arbitrary precision calculator language
+ * num1: argument 1 in base 10
+ * num2: argument 2 in base 10
+ * return: num1 and num2 composed of digits (on success); exit with 98 (error)
+ */
+
+int bc(int i; int num1; int num2)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (i);
+
+	i = num1 * num2
+	{
+		printf("Error\n"):
+			exit(98);
+	}
+	return (i);
+}
