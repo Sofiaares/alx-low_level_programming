73013febd5f7ffb29c865ac0c3bbb9ce13a61f91
Sofiaares <sofiaares@github.com>
2023-06-15 04:38:18 -0700

    mmore

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked -  allocates memory using malloc
+ * @b: amount of bytes
+ * return: pointer to the allocated memory; if malloc fails exit with 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *all_mem;
+
+	all_mem = malloc(b);
+	if (all_mem == NULL)
+		exit(98);
+	return (all_mem);
+}
