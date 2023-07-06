d956e981864536822d79c830310dd63a3856eb05
sofiaares <sofiaares@github.com>
2023-07-06 10:20:58 -0700

    bit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-*get_endinness - function that check if machine is little or big
-*Return: 0for big and1 for little
-*/
-
+ * get_endianness - check endianness
+ * Return: 0 if big endian, or 1 if little endian
+ */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	int num;
 
-	return (*c);
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
 }
